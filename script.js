d15b93277b52c25eb7ffe44906e857844c2ba303
alex <alexsolis1807@gmail.com>
2026-03-04 04:59:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,22 @@ $(document).ready(function () {
 
   function open() {
     envelope.addClass("open").removeClass("close");
+     createShootingStar(); // 🌠 estrella al abrir
   }
+  
   function close() {
     envelope.addClass("close").removeClass("open");
+  }
+  
+  function createShootingStar(){
+    const star = document.createElement("div");
+    star.classList.add("shooting-star");
+    document.body.appendChild(star);
+
+    star.style.animation = "shoot 1.5s ease-out forwards";
+
+    setTimeout(function(){
+      star.remove();
+    },1500);
   }
 });
